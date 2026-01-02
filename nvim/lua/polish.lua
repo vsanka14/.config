55f1ca3abf3681af5691d84ae66f0937b1c086fb
Vasisht Shankar <vsankar@linkedin.com>
2026-01-02 15:56:26 -0800

    font italics for keywords in nvim

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -15,3 +15,47 @@ vim.filetype.add({
 
 -- Use markdown treesitter parser for MDX files
 vim.treesitter.language.register("markdown", "mdx")
+
+-- ============================================================================
+-- Italic Highlights (merges with existing colorscheme colors)
+-- ============================================================================
+-- Helper function to add italic to existing highlight groups
+local function italicize(group)
+  local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
+  hl.italic = true
+  vim.api.nvim_set_hl(0, group, hl)
+end
+
+-- Apply italics after colorscheme loads
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- Comments
+    italicize("Comment")
+
+    -- Keywords
+    italicize("@keyword")
+    italicize("@keyword.return")
+    italicize("@keyword.function")
+    italicize("@keyword.operator")
+    italicize("@keyword.conditional")
+    italicize("@keyword.repeat")
+    italicize("@keyword.import")
+
+    -- this/self
+    italicize("@variable.builtin")
+  end,
+})
+
+-- Also apply immediately for the current session
+vim.defer_fn(function()
+  italicize("Comment")
+  italicize("@keyword")
+  italicize("@keyword.return")
+  italicize("@keyword.function")
+  italicize("@keyword.operator")
+  italicize("@keyword.conditional")
+  italicize("@keyword.repeat")
+  italicize("@keyword.import")
+  italicize("@variable.builtin")
+end, 0)
