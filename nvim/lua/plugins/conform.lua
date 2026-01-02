8754ded0d02a16ff6cf19272d4ac08920fe0d7f4
Vasisht Sankar <vsankar@vsankar-mn1888.linkedin.biz>
2026-01-01 19:36:45 -0800

    hbs support

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -16,8 +16,20 @@ return {
       yaml = { "prettier" },
       markdown = { "prettier" },
       mdx = { "prettier" },
+      handlebars = { "prettier" },
       lua = { "stylua" },
       gitignore = { "trim_whitespace" },
+    },
+    formatters = {
+      prettier = {
+        prepend_args = function(self, ctx)
+          -- Use glimmer parser for .hbs files
+          if vim.fn.expand("%:e") == "hbs" then
+            return { "--parser", "glimmer" }
+          end
+          return {}
+        end,
+      },
     },
     format_on_save = {
       timeout_ms = 1000,
