5e4e4d431f6be4b0437f87823a7979ba17b44e92
Vasisht Shankar <vsankar@linkedin.com>
2026-01-07 18:26:12 -0800

    Revert "move all custom plugin config into user.lua"
    This reverts commit 6e4055d26fff92df169bff9d4d28c71430281184.

---
+++ nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,16 @@
+-- Disable blink.cmp completion for markdown and text files
+
+---@type LazySpec
+return {
+  "saghen/blink.cmp",
+  opts = {
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      per_filetype = {
+        markdown = {}, -- No completion sources for markdown
+        mdx = {}, -- No completion sources for MDX files
+        text = {}, -- No completion sources for text files
+      },
+    },
+  },
+}
