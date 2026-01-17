279e9abef9af26d9b89240cbe38bac375889f34d
Vasisht Shankar <vsankar@linkedin.com>
2026-01-16 18:17:18 -0800

    sketchybar: battery + clock updates

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+BATT_INFO=$(pmset -g batt)
+PERCENTAGE=$(echo "$BATT_INFO" | grep -Eo "\d+%" | cut -d% -f1)
+CHARGING=$(echo "$BATT_INFO" | grep -q "AC Power" && echo "true" || echo "false")
 
 if [ "$PERCENTAGE" = "" ]; then
     exit 0
 fi
 
-sketchybar --set $NAME label="${PERCENTAGE}%"
+# Select icon based on charging state and battery level
+if [ "$CHARGING" = "true" ]; then
+    ICON="󰂄"  # Charging icon
+elif [ "$PERCENTAGE" -ge 100 ]; then
+    ICON="󰁹"  # Full
+elif [ "$PERCENTAGE" -ge 75 ]; then
+    ICON="󰂀"  # High
+elif [ "$PERCENTAGE" -ge 50 ]; then
+    ICON="󰁾"  # Medium
+elif [ "$PERCENTAGE" -ge 25 ]; then
+    ICON="󰁼"  # Low
+elif [ "$PERCENTAGE" -ge 10 ]; then
+    ICON="󰁻"  # Critical low
+else
+    ICON="󰂃"  # Alert
+fi
+
+sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
--- sketchybar/plugins/clock.sh
+++ sketchybar/plugins/clock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sketchybar --set $NAME label="$(date '+%a %b %d  %I:%M %p')"
+sketchybar --set $NAME icon="󰃭" label="$(date '+%a %b %d  %I:%M %p')"
