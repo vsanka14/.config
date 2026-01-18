879301a9fe9cd8b3a187c427f562f5c78a634b09
Vasisht Shankar <vsankar@linkedin.com>
2026-01-17 22:23:28 -0800

    Sketchybar: Updated animation to fix lingering hover bg

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -14,20 +14,24 @@ ITEM_BG_COLOR=0xcc1e2030          # Moon background
 # Get focused workspace from aerospace
 FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
 
-# Extract space number from item name (space.1 -> 1)
-SPACE_NUM=${NAME#space.}
-
-if [ "$SPACE_NUM" = "$FOCUSED_WORKSPACE" ]; then
-    # Pop up instantly, then animate back down (bounce effect)
-    sketchybar --set $NAME icon.y_offset=3 \
-        --animate tanh 20 --set $NAME \
-        icon.color=${WORKSPACE_COLORS[$SPACE_NUM]} \
-        icon.y_offset=0 \
-        background.drawing=on \
-        background.color=$ITEM_BG_COLOR
-else
-    sketchybar --animate tanh 15 --set $NAME \
-        icon.color=$FG_MUTED \
-        icon.y_offset=0 \
-        background.drawing=off
-fi
+# Update all spaces based on current focus
+# Background changes are instant (before --animate), icon changes are animated
+for i in 1 2 3 4 5; do
+    if [ "$i" = "$FOCUSED_WORKSPACE" ]; then
+        # Focused space: instant background, animate icon bounce
+        sketchybar --set space.$i \
+            icon.y_offset=3 \
+            background.drawing=on \
+            background.color=$ITEM_BG_COLOR \
+            --animate tanh 20 --set space.$i \
+            icon.color=${WORKSPACE_COLORS[$i]} \
+            icon.y_offset=0
+    else
+        # Non-focused: instant background off, animate icon
+        sketchybar --set space.$i \
+            background.drawing=off \
+            --animate tanh 15 --set space.$i \
+            icon.color=$FG_MUTED \
+            icon.y_offset=0
+    fi
+done
