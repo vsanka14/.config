1546a9856db24e58cf4977512e0a8f6d37ed38f9
Vasisht Shankar <vsankar@linkedin.com>
2026-02-01 17:30:53 -0800

    Nvim: smoother smearcursor

--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -1,11 +1,10 @@
 return {
   "sphamba/smear-cursor.nvim",
-  lazy = true,
-  event = "VeryLazy", -- Load after startup is complete
-  -- Smooth cursor - optimized for speed
+
   opts = {
     stiffness = 0.5,
     trailing_stiffness = 0.5,
     matrix_pixel_threshold = 0.5,
+    time_interval = 7,
   },
 }
